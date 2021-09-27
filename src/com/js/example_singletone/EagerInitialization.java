cf6d390decd98e44588c19dfa2e45005ce050710
j222su <patrickstar0910@gmail.com>
2021-09-27 18:33:46 +0900

    RENAME : value name

--- src/com/js/example_singletone/EagerInitialization.java
+++ src/com/js/example_singletone/EagerInitialization.java
@@ -2,16 +2,13 @@ package com.js.example_singletone;
 
 public class EagerInitialization {
 
-    //싱글톤 객체를 미리 만들어 놓는다.
-    private static EagerInitialization eagerInitialization = new EagerInitialization();
+    private static EagerInitialization instance = new EagerInitialization();
 
-    //다른 곳에서 객체를 생성할 수 없도록 접근제어자를 'private'로 한다.
     private EagerInitialization() {
 
     }
 
-    //싱글톤 객체를 리턴 받는 함수
-    public static EagerInitialization getEagerInitialization() {
-        return eagerInitialization;
+    public static EagerInitialization getInstance() {
+        return instance;
     }
 }
